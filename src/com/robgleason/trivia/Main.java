005649a88dd0f91cfbde6ac2e9d0d6f4d6e1502d
Robert Gleason <l2ob122@gmail.com>
2022-09-01 21:58:00 -0700

    Added 3 questions

--- src/com/robgleason/trivia/Main.java
+++ src/com/robgleason/trivia/Main.java
@@ -13,13 +13,19 @@ public class Main {
                 "d) Italy");
         String question1 = scan.nextLine();
 
-        System.out.println("1. Which planet is the hottest?\n" +
+        System.out.println("2. Which planet is the hottest?\n" +
                 "a) Venus\n" +
                 "b) Saturn\n" +
                 "c) Mercury\n" +
                 "d) Mars");
         String question2 = scan.nextLine();
-        
+
+        System.out.println("3. WHat is the rarest blood type?\n" +
+                "a) O\n" +
+                "b) A\n" +
+                "c) B\n" +
+                "d) AB-Negative");
+
 
     }
 }
