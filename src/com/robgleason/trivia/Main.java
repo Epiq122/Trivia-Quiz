09753dc157f1dba247f1da441d301541b3383030
Robert Gleason <l2ob122@gmail.com>
2022-09-01 21:51:50 -0700

    Added 2 questions

--- src/com/robgleason/trivia/Main.java
+++ src/com/robgleason/trivia/Main.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
+        System.out.println("1. Which country held the 2016 Summer Olympics?\n" +
+                "a) China\n" +
+                "b) Ireland\n" +
+                "c) Brazil\n" +
+                "d) Italy");
+        String question1 = scan.nextLine();
+
+        System.out.println("1. Which planet is the hottest?\n" +
+                "a) Venus\n" +
+                "b) Saturn\n" +
+                "c) Mercury\n" +
+                "d) Mars");
+        String question2 = scan.nextLine();
+
     }
 }
